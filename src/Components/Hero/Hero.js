7502f1d57a495632508112d92a97d6adf708fa4a
Kripesh <kripeshn90@gmail.com>
2026-01-23 20:03:29 +0545

    update

--- src/Components/Hero/Hero.js
+++ src/Components/Hero/Hero.js
@@ -7,8 +7,8 @@ const Hero = () => {
   return (
     <div id='home' className='hero'>
         <img src={profile} alt=''/>
-        <h1><span>Hi! I'm Kripesh Neupane</span>, a frontend developer.</h1>
-        <p>I am a student currently pursuing a B.Sc. in Computer Science and Information Technology (CSIT) degree and is passionate about frontend development</p>
+        <h1><span>Hi! I'm Kripesh Neupane</span>, a fullstack developer.</h1>
+        <p>I am a student currently pursuing a B.Sc. in Computer Science and Information Technology (CSIT) degree and is passionate about fullstack development</p>
         <div className='hero-action'>
             <div className='hero-connect'><AnchorLink className='anchor-link' offset={50} href='#contact'>Connect with me</AnchorLink></div>
             <a className='hero-resume' href={resume} target='blank'>Resume</a>
